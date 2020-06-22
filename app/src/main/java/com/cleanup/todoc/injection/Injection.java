5c4c426c96009360282d6ea1da60fedc701969f1
Edouard Loussouarn <ed.loussouarn@gmail.com>
2020-06-22 10:10:05 +0200

    Injection Package

---
+++ app/src/main/java/com/cleanup/todoc/injection/Injection.java
@@ -0,0 +1,32 @@
+package com.cleanup.todoc.injection;
+
+import android.content.Context;
+
+import com.cleanup.todoc.database.ToDocDatabase;
+import com.cleanup.todoc.repositorie.ProjectDataRepository;
+import com.cleanup.todoc.repositorie.TaskDataRepository;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class Injection {
+
+    public static TaskDataRepository provideTaskDataSource(Context context) {
+        ToDocDatabase database = ToDocDatabase.getInstance(context);
+        return new TaskDataRepository(database.taskDao());
+    }
+
+    public static ProjectDataRepository provideProjectDataSource(Context context) {
+        ToDocDatabase database = ToDocDatabase.getInstance(context);
+        return new ProjectDataRepository(database.projectDao());
+    }
+
+    public static Executor provideExecutor(){ return Executors.newSingleThreadExecutor(); }
+
+    public static ViewModelFactory provideViewModelFactory(Context context) {
+        TaskDataRepository dataSourceTask = provideTaskDataSource(context);
+        ProjectDataRepository dataSourceProject = provideProjectDataSource(context);
+        Executor executor = provideExecutor();
+        return new ViewModelFactory(dataSourceTask, dataSourceProject, executor);
+    }
+}
---
+++ app/src/main/java/com/cleanup/todoc/injection/ViewModelFactory.java
@@ -0,0 +1,32 @@
+package com.cleanup.todoc.injection;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.cleanup.todoc.repositorie.ProjectDataRepository;
+import com.cleanup.todoc.repositorie.TaskDataRepository;
+import com.cleanup.todoc.ui.TaskViewModel;
+
+import java.util.concurrent.Executor;
+
+public class ViewModelFactory implements ViewModelProvider.Factory {
+
+    private final TaskDataRepository taskDataSource;
+    private final ProjectDataRepository projectDataSource;
+    private final Executor executor;
+
+    public ViewModelFactory(TaskDataRepository taskDataSource, ProjectDataRepository projectDataSource, Executor executor) {
+        this.taskDataSource = taskDataSource;
+        this.projectDataSource = projectDataSource;
+        this.executor = executor;
+    }
+
+
+    @Override
+    public <T extends ViewModel> T create(Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(TaskViewModel.class)) {
+            return (T) new TaskViewModel(taskDataSource, projectDataSource, executor);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel Class");
+    }
+}
